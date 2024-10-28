e028fea6a7bc7495811c621c6d656e2042262575
Israel Valderrama <ivalgar260@g.educaand.es>
2024-10-28 09:22:06 +0100

    Update fuerza_bruta.py

--- source/fuerza_bruta.py
+++ source/fuerza_bruta.py
@@ -4,15 +4,28 @@ def calcula_sha(passwd: str):
     m = hashlib.sha256(passwd.encode())
     return m.hexdigest()
 
-def comparacion_sha(passwd:str,archivo:str)->bool:
-    with open(archivo,'r') as f:
-        contenido = f.read()
-        contenido_sha=calcula_sha(contenido)
-        if(contenido_sha==passwd):
-            return True
-        else:
-            return False
+
+def comparacion_sha(passwd:str,archivo:str):
+    
+    with open(archivo, 'r') as f:
+        lines = f.readlines()
+        for line_number, line in enumerate(lines, start=1):
+            contenido = line.strip()
+            contenido_sha = calcula_sha(contenido)            
+            
+            if contenido_sha == passwd:
+                
+                return True, contenido
+    
+    return False,None
 
 if __name__=='__main__':
-   print (comparacion_sha(calcula_sha('root'),'contrasenas_test.txt'))
+   
+   contraseña_sha= input('Dime un sha para buscar su contraseña en el diccionario\n')
+   comparacion, contenido=  comparacion_sha(contraseña_sha,'contrasenas.txt')
+   if comparacion:
+    print(f'La contraseña para el sha {contraseña_sha} es {contenido} ')
+   else:
+      print(f'La contraseña para el sha {contraseña_sha} no se ha encontrado en la lista')
+
 
